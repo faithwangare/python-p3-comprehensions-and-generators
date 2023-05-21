c30d8949484e7998a490a09efff9c5c7c9768c0a
faith wangare <faithwangare750@gmail.com>
2023-05-21 21:49:23 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    return [j for j in num_list if j % 2 == 0]
+return_evens([0, 1, 3, 5, 7, 8, 9])
 def make_exclamation(sentence_list):
-    pass
+    return [j + '!' for j in sentence_list]
